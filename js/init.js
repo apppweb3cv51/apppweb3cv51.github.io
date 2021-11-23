a9eb282ea2791641dc476fcf3975b0c7cab76d99
apppweb3cv51 <94923785+apppweb3cv51@users.noreply.github.com>
2021-11-23 11:27:49 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyBAPXj_zSqx2LimfCqr0qhHTiyTnnsm9TQ",
-   authDomain: "apppweb3cv511.firebaseapp.com",
-   projectId: "apppweb3cv511",
-   storageBucket: "apppweb3cv511.appspot.com",
-   messagingSenderId: "636784745650",
-   appId: "1:636784745650:web:67b1dda134cd2422327dab",
-   measurementId: "G-R76CDD1W4H
+ apiKey: "AIzaSyBAPXj_zSqx2LimfCqr0qhHTiyTnnsm9TQ",
+  authDomain: "apppweb3cv511.firebaseapp.com",
+  projectId: "apppweb3cv511",
+  storageBucket: "apppweb3cv511.appspot.com",
+  messagingSenderId: "636784745650",
+  appId: "1:636784745650:web:67b1dda134cd2422327dab",
+  measurementId: "G-R76CDD1W4H
 });
